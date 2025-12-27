4aabe308f9f1b7c944c29233decad0ebb17bc707
DanielVerissimo <danielsantoss1300@gmail.com>
2025-12-27 14:14:09 -0300

    fix: nomeclatura do tipo de arquivo

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -133,7 +133,7 @@ const AboutSection = () => {
                 <div className="absolute inset-0 border-4 border-white/20 rounded-full z-20 transition-all duration-700 group-hover:border-white/40 group-hover:scale-105" />
                 
                 <Image
-                  src="/profile/Imagem-daniel03.jpg"
+                  src="/profile/Imagem-daniel03.JPG"
                   alt="Profile"
                   fill
                   className="object-cover object-center transition-all duration-700 group-hover:scale-110"
