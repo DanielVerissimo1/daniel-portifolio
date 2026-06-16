f7f1e5b1a5e73cd9a04f52487454d0a86625a825
danielVerissimo <danielsantoss1300@gmail.com>
2026-06-16 10:11:27 -0300

    chore: fazendo ajustes

--- src/data/Stack.ts
+++ src/data/Stack.ts
@@ -11,9 +11,10 @@ export const techStacks: TechStack[] = [
   { icon: "/tech-stack/postgresql.svg", language: "PostgreSQL" },
   { icon: "/tech-stack/docker.svg", language: "Docker" },
   { icon: "/tech-stack/prisma.svg", language: "Prisma" },
-  { icon: "/tech-stack/fastify.svg", language: "Fastify" },
+  { icon: "/tech-stack/express-js.svg", language: "Express" },
   { icon: "/tech-stack/React-Query.svg", language: "TanStack" },
   { icon: "/tech-stack/vite.svg", language: "Vite" },
   { icon: "/tech-stack/next.svg", language: "Next.js" },
-  { icon: "/tech-stack/tailwind.svg", language: "Tailwind CSS" }
+  { icon: "/tech-stack/tailwind.svg", language: "Tailwind CSS" },
+  { icon: "/tech-stack/Jest.svg", language: "Jest" }
 ];
